ccf18f723d6db20bae8597f63d70f7993e03291c
Aiji Uejima <aiji42@gmail.com>
2021-06-26 15:54:27 +0900

    create src/split-challenge.ts

---
+++ src/split-challenge.ts
@@ -0,0 +1,18 @@
+import { GetServerSideProps } from 'next'
+import { setCookie } from 'nookies'
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const branches: string[] = JSON.parse(process.env.SPLIT_TEST_BRANCHES ?? '[]')
+  setCookie(
+    ctx,
+    'next-with-split',
+    branches[Math.floor(Math.random() * branches.length)],
+    { path: '/' }
+  )
+  ctx.res.writeHead(302, { Location: ctx.req.url ?? '/' })
+  ctx.res.end()
+
+  return {
+    props: {}
+  }
+}
