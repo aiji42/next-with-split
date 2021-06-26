7b4bb50dfefb7d6af4985a594d98024562db0c13
Aiji Uejima <aiji42@gmail.com>
2021-06-26 15:55:16 +0900

    create src/check-existing-split-challenge.ts

---
+++ src/check-existing-split-challenge.ts
@@ -0,0 +1,29 @@
+import { findPageFile } from 'next/dist/server/lib/find-page-file'
+import { findPagesDir } from 'next/dist/lib/find-pages-dir'
+import { error, info } from './log'
+
+export const checkExistingSplitChallenge = async (): Promise<boolean> => {
+  const pagesDir = findPagesDir('')
+  const res = await findPageFile(findPagesDir(''), '_split-challenge', [
+    'mdx',
+    'jsx',
+    'js',
+    'ts',
+    'tsx'
+  ])
+
+  if (res) return true
+  error(
+    `Not existing ${pagesDir}/_split-challenge.js(ts)}.`
+  )
+  info(
+    `Plese create ${pagesDir}/_split-challenge.js or .ts and and copy and paste the following three lines.`
+  )
+  console.log(`
+    export { getServerSideProps } from 'next-with-split'
+    const SplitChallenge = () => null
+    export default SplitChallenge
+`)
+  return false
+}
+
