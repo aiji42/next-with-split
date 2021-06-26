98f13d16528d41c9d69730f68dbeadabfcff158b
Aiji Uejima <aiji42@gmail.com>
2021-06-26 17:46:11 +0900

    update src/check-existing-split-challenge.ts

--- src/check-existing-split-challenge.ts
+++ src/check-existing-split-challenge.ts
@@ -14,7 +14,7 @@ export const checkExistingSplitChallenge = async (): Promise<boolean> => {
 
   if (res) return true
   error(
-    `Not existing ${pagesDir}/_split-challenge.js(ts)}.`
+    `Not existing ${pagesDir}/_split-challenge.js(ts).`
   )
   info(
     `Plese create ${pagesDir}/_split-challenge.js or .ts and and copy and paste the following three lines.`
