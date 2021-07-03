50d27036b7f3ec08f5d4ebef6d0d28a577e8e53f
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-07-03 22:42:38 +0900

    wip

--- example/next.config.js
+++ example/next.config.js
@@ -1,13 +1,23 @@
 module.exports = {
+  serverRuntimeConfig: {
+    splits: {
+      test1: {
+        original: { host: 'lifedot-list-o3j04lux4-ending.vercel.app', path: '/ohaka/pref-:pref(tokyo|aichi)/:path*/list/', cookie: {
+            path: '/',
+            maxAge: 10
+          } },
+        challenger: { host: 'lifedot-list-o3j04lux4-ending.vercel.app', path: '/ohaka/pref-:pref(tokyo|aichi)/:path*/list/', cookie: {
+            path: '/',
+            maxAge: 10
+          } }
+      }
+    }
+  },
   rewrites: async () => ({
     beforeFiles: [
       {
-        source: '/ohaka/pref-tokyo/list/',
-        destination: '/_split-challenge',
-      },
-      {
-        source: '/ohaka/pref-tokyo/list',
-        destination: '/_split-challenge',
+        source: '/ohaka/pref-:pref(tokyo|aichi)/:path*/list/',
+        destination: '/_split-challenge/test1',
       }
     ]
   }),
