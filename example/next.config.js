946aaa99f26477beadb4d5b85ea57c7a77078996
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-07-04 16:44:49 +0900

    chore: update example

--- example/next.config.js
+++ example/next.config.js
@@ -3,10 +3,10 @@ const { withSplit } = require('next-with-split')
 module.exports = withSplit({
   splits: {
     test1: {
-      path: '/ohaka/pref-:pref(tokyo|aichi)/:path*/list/',
+      path: '/foo/:path*/',
       hosts: {
-        branch1: 'lifedot-list-o3j04lux4-ending.vercel.app',
-        branch2: 'lifedot-list-o3j04lux4-ending.vercel.app'
+        branch1: 'example1.vercel.app',
+        branch2: 'example2.vercel.app'
       }
     }
   },
