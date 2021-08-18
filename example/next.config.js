7e8359746aa02feeb1873af2a8d5431a3a9a6352
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-08-18 22:56:51 +0900

    chore: update example

--- example/next.config.js
+++ example/next.config.js
@@ -1,26 +1,26 @@
 /* eslint @typescript-eslint/no-var-requires: 0 */
 const withSplit = require('next-with-split')({
-  splits: {
-    test1: {
-      path: '/foo/abtest/:path*',
-      hosts: {
-        original: { host: 'http://localhost:3000', weight: 3 },
-        // original: 'https://nextjs-split-test-git-abtest-original-2021-07-04-aiji42.vercel.app',
-        challenger:
-          'https://nextjs-split-test-git-abtest-example-2021-07-04-aiji42.vercel.app'
-      }
-    },
-    test2: {
-      path: '/top',
-      hosts: {
-        original: 'http://localhost:3000',
-        challenger:
-          'https://nextjs-split-test-git-abtest-example-2021-07-04-aiji42.vercel.app'
-      }
-    }
-  },
-  prepared: true,
-  isOriginal: true
+  // splits: {
+  //   test1: {
+  //     path: '/foo/abtest/:path*',
+  //     hosts: {
+  //       original: { host: 'http://localhost:3000', weight: 3 },
+  //       // original: 'https://nextjs-split-test-git-abtest-original-2021-07-04-aiji42.vercel.app',
+  //       challenger:
+  //         'https://nextjs-split-test-git-abtest-example-2021-07-04-aiji42.vercel.app'
+  //     }
+  //   },
+  //   test2: {
+  //     path: '/top',
+  //     hosts: {
+  //       original: 'http://localhost:3000',
+  //       challenger:
+  //         'https://nextjs-split-test-git-abtest-example-2021-07-04-aiji42.vercel.app'
+  //     }
+  //   }
+  // },
+  // prepared: true,
+  // isOriginal: true
 })
 
 module.exports = withSplit({
