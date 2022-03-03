e2a8bd087d3daef953afbbcf2f4053676df5f759
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-03-04 00:38:21 +0900

    chore: update example

--- example/pages/foo/_middleware.ts
+++
@@ -1,4 +0,0 @@
-// This file was installed automatically by next-with-split.
-// Note: Do not update this file manually.
-// See https://github.com/aiji42/next-with-split/tree/main#auto-installremove-middleware-file
-export { middleware } from 'next-with-split'
