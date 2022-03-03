7f55ed95fcea5d851581d4819da72de98d5ab80a
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-03-04 00:35:50 +0900

    chore: bumpup example

---
+++ example/pages/foo/_middleware.ts
@@ -0,0 +1,4 @@
+// This file was installed automatically by next-with-split.
+// Note: Do not update this file manually.
+// See https://github.com/aiji42/next-with-split/tree/main#auto-installremove-middleware-file
+export { middleware } from 'next-with-split'
