9eef8608d0330b73836eac17aa21abb4cc3effbf
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-04-04 11:37:17 +0900

    chore: bumpup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  test: {
-    coverage: {
-      reporter: ['text', 'json', 'html', 'lcov']
-    }
-  }
+  test: {}
 })
